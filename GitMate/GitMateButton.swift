83e6383cf464e368105b9b73b0f2decb0c8dcb9c
Min Pyae <minmp2002@gmail.com>
2024-06-04 09:30:42 +0100

    Edit SearchViewController and Create GitMateButton and GitMateTextField

---
+++ GitMate/GitMateButton.swift
@@ -0,0 +1,35 @@
+//
+//  GitMateButton.swift
+//  GitMate
+//
+//  Created by Chris Min on 31/05/2024.
+//
+
+import UIKit
+
+class GitMateButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
